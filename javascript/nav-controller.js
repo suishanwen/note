f52ecc442bb9213f349469a5c0d45ee3ac88f9ae
suishanwen <526253340@qq.com>
2016-04-13 12:19:24 +0800

    add nav bg

--- javascript/nav-controller.js
+++ javascript/nav-controller.js
@@ -1,6 +1,13 @@
 var NavController = function ($scope, $location, noteService) {
+    setBgHeight();
+    $(window).resize(function(){
+        setBgHeight();
+    });
     $scope.toDoSomething = function (loc) {
         noteService.note=null;
         $location.path('/' + loc)
     };
+    function setBgHeight(){
+        $(".nav-main")[0].style.height = window.innerHeight+'px';
+    }
 };
